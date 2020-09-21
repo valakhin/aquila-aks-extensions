e90225d3c4a82e9b5cd3957b56e577025edcba53
Vasudevu Lakhinana <valakhin@microsoft.com>
2020-09-21 08:48:16 +0530

    Added extension for gmsa webhook installation

--- extensions/setup-gmsawebhook/v1/installwebhook.sh
+++ extensions/setup-gmsawebhook/v1/installwebhook.sh
@@ -6,35 +6,35 @@
 
 
 download_install_gmsa_crd() {
-	local URL="https://raw.githubusercontent.com/valakhin/aquila-aks-extensions/master/extensions/setup-gmsawebhook/v1/gmsa-crd.yaml"
-	if which curl &> /dev/null; then
-	   curl -sL "$URL" > "gmsa-crd.yaml"
-	else
-	   wget -O "gmsa-crd.yaml" "$URL"
-	fi
+  local URL="https://raw.githubusercontent.com/valakhin/aquila-aks-extensions/master/extensions/setup-gmsawebhook/v1/gmsa-crd.yaml"
+  if which curl &> /dev/null; then
+    curl -sL "$URL" > "gmsa-crd.yaml"
+  else
+    wget -O "gmsa-crd.yaml" "$URL"
+  fi
 
-	#local CRD_MANIFEST_PATH=$(ensure_helper_file_present 'gmsa-crd.yml')
-	#local CRD_MANIFEST_CONTENTS=$(cat "$CRD_MANIFEST_PATH")
-	if kubectl get crd gmsacredentialspecs.windows.k8s.io &> /dev/null; then
-		kubectl delete crd gmsacredentialspecs.windows.k8s.io
-	fi
-	kubectl create -f gmsa-crd.yaml
-	
+  #local CRD_MANIFEST_PATH=$(ensure_helper_file_present 'gmsa-crd.yml')
+  #local CRD_MANIFEST_CONTENTS=$(cat "$CRD_MANIFEST_PATH")
+  if kubectl get crd gmsacredentialspecs.windows.k8s.io &> /dev/null; then
+    kubectl delete crd gmsacredentialspecs.windows.k8s.io
+  fi
+  kubectl create -f gmsa-crd.yaml
+
 }	
 
 download_deploy_gmsa_webhook() {
 
-local URL="https://raw.githubusercontent.com/kubernetes-sigs/windows-gmsa/master/admission-webhook/deploy/deploy-gmsa-webhook.sh"
+  local URL="https://raw.githubusercontent.com/kubernetes-sigs/windows-gmsa/master/admission-webhook/deploy/deploy-gmsa-webhook.sh"
 
-if which curl &> /dev/null; then
-   curl -sL "$URL" > "deploy-gmsa-webhook.sh"
-else
-   wget -O "deploy-gmsa-webhook.sh" "$URL"
-fi
+  if which curl &> /dev/null; then
+    curl -sL "$URL" > "deploy-gmsa-webhook.sh"
+  else
+    wget -O "deploy-gmsa-webhook.sh" "$URL"
+  fi
 
-chmod +777 deploy-gmsa-webhook.sh
+  chmod +777 deploy-gmsa-webhook.sh
 
-./deploy-gmsa-webhook.sh ~/.kube/gmsa-webhook-manifest.yaml
+  ./deploy-gmsa-webhook.sh --file ~/.kube/gmsa-webhook-manifest.yaml
 
 }
 
